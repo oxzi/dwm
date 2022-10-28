d9edbfb72dbfd92eba1dc17a7fe77d3519b889d7
Hiltjo Posthuma <hiltjo@codemadness.org>
2022-10-28 16:37:56 +0200

    Revert "Remove dmenumon variable"
    This reverts commit c2b748e7931e5f28984efc236f9b1a212dbc65e8.
    
    Revert back this change. It seems to not be an edge-case anymore since
    multiple users have asked about this new behaviour now.

--- config.def.h
+++ config.def.h
@@ -56,7 +56,6 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-
 static const char *dmenucmd[]        = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray_norm , "-sb", col_gray1 , "-sf", col_gray_sel, NULL };
 static const char *termcmd[]         = { "st", NULL };
 static const char *slockcmd[]        = { "slock", NULL };
--- dwm.c
+++ dwm.c
@@ -1877,6 +1877,8 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
+	if (arg->v == dmenucmd)
+		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
