2258479ae029547bdb32540cd9b321589ead96af
Alvar Penning <alvar.penning@icinga.com>
2026-05-06 10:51:10 +0200

    config.def.h: Spawn new windows on current display

--- config.def.h
+++ config.def.h
@@ -28,7 +28,14 @@ static const char *colors[][3]           = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7" };
 
-static const Rule rules[] = {0};
+static const Rule rules[] = {
+  /* xprop(1):
+   *  WM_CLASS(STRING) = instance, class
+   *  WM_NAME(STRING) = title
+   */
+  /* class      instance    title       tags mask     isfloating   monitor */
+  { NULL,       NULL,       NULL,       0,            0,           -1 },
+};
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
